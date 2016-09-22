888ae645cd02535c6a2aa6c92a618c7207948426
N-Avinash <nethalaavinash94@gmail.com>
2016-09-22 21:42:09 +0530

    Biggest and smallest of 3 numbers using lists

---
+++ biggest_smallest_list_3numbers.py
@@ -0,0 +1,10 @@
+__author__ = 'Avinash'
+
+number1 = int(input('Enter First number : '))
+number2 = int(input('Enter Second number : '))
+number3 = int(input('Enter Third number : '))
+
+lst = [number1, number2, number3]
+
+print("The largest of the 3 numbers is : ", max(lst))
+print("The smallest of the 3 numbers is : ", min(lst))
