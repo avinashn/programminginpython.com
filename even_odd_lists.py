48d0537711d28ec2ca17088021341a1d07307ff2
N-Avinash <nethalaavinash94@gmail.com>
2017-11-21 23:05:13 +0530

    Separate even and odd numbers in a list

---
+++ even_odd_lists.py
@@ -0,0 +1,21 @@
+__author__ = 'Avinash'
+
+numbers = []
+n = int(input("Enter number of elements: \t"))
+
+for i in range(1, 1+n):
+    allElements = int(input("Enter element: \t"))
+    numbers.append(allElements)
+
+even_lst = []
+odd_lst = []
+
+for j in numbers:
+    if j % 2 == 0:
+        even_lst.append(j)
+    else:
+        odd_lst.append(j)
+
+print("Even numbers list \t", even_lst)
+print("Odd numbers list \t", odd_lst)
+
