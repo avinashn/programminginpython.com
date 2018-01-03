e941764287c2e896eec2d1e95fb51d7b907f925d
N-Avinash <nethalaavinash94@gmail.com>
2018-01-03 12:01:00 +0530

    Algorithms - Search - Linear Search

---
+++ Algorithms/Search Algorithms/linear_search.py
@@ -0,0 +1,19 @@
+__author__ = 'Avinash'
+
+lst = []
+num = int(input("Enter size of list: \t"))
+for n in range(num):
+    numbers = int(input("Enter any number: \t"))
+    lst.append(numbers)
+
+x = int(input("\nEnter number to search: \t"))
+
+found = False
+
+for i in range(len(lst)):
+    if lst[i] == x:
+        found = True
+        print("\n%d found at position %d" % (x, i))
+        break
+if not found:
+    print("\n%d is not in list" % x)
