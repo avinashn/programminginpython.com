918e7f6a3da8e1ac2cfc3012ef8f689fcd113601
Avinash <nethalaavinash94@gmail.com>
2018-03-09 09:57:26 +0530

    formatted to pep 8 and updated Readme

--- Algorithms/Sorting Algorithms/merge_sort.py
+++ Algorithms/Sorting Algorithms/merge_sort.py
@@ -1,16 +1,16 @@
 __author__ = 'Avinash'
- 
- 
+
+
 def merge_sort(sort_list):
     print("splitting", sort_list)
     if len(sort_list) > 1:
         mid = len(sort_list) // 2
         leftHalf = sort_list[:mid]
         rightHalf = sort_list[mid:]
- 
+
         merge_sort(leftHalf)
         merge_sort(rightHalf)
- 
+
         i = 0
         j = 0
         k = 0
@@ -22,24 +22,24 @@ def merge_sort(sort_list):
                 sort_list[k] = rightHalf[j]
                 j = j + 1
             k = k + 1
- 
+
         while i < len(leftHalf):
             sort_list[k] = leftHalf[i]
             i = i + 1
             k = k + 1
- 
+
         while j < len(rightHalf):
             sort_list[k] = rightHalf[j]
             j = j + 1
             k = k + 1
     print("merging...", sort_list)
- 
- 
+
+
 lst = []
 size = int(input("Enter size of the list: \t"))
- 
+
 for i in range(size):
     elements = int(input("Enter an element: \t"))
     lst.append(elements)
- 
+
 merge_sort(lst)
