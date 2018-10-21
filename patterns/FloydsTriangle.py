609441d3b76b770a07d2e69edd0c5b3bcd870924
Avinash <nethalaavinash94@gmail.com>
2018-10-21 19:59:22 +0530

    Floyd's Triangle Pattern

--- patterns/FloydsTriangle.py
+++ patterns/FloydsTriangle.py
@@ -1,15 +1,27 @@
 __author__ = 'Avinash'
 
+# Print a Floyd's Triangle
+
+# Range of the triangle
 size = int(input("Enter the range: \t "))
+
+print("\nFLOYD'S TRIANGLE with numbers: \n")
 k = 1
 
+# 2 for loops, one for column looping another for row looping
+# i loop for column looping and j loop for row looping
 for i in range(1, size + 1):
     for j in range(1, i + 1):
         print(k, end=" ")
         k = k + 1
-    print("\n")
+    print()
+print("\n")
 
+
+# Print using *'s
+print("\nFLOYD'S TRIANGLE with *'s: \n")
 for i in range(1, size + 1):
     for j in range(1, i + 1):
         print('*', end=" ")
-    print("\n")
+    print()
+print("\n")
