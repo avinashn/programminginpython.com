e4ef548fd3ce3a5fcacd51b3bbb110c06a70e717
avinashn <nethalaavinash94@gmail.com>
2019-03-31 17:33:39 +0530

    armstrong number bug fix

--- armstrong.py
+++ armstrong.py
@@ -1,13 +1,14 @@
 __author__ = 'Avinash'
 
 input_num = (input("Enter any number: "))
+digit_len = len(str(input_num))
 
 try:
     arm_num = 0
     val = int(input_num)
     while val > 0:
         reminder = val % 10
-        arm_num += reminder ** 3
+        arm_num += reminder ** digit_len
         val //= 10
 
     if int(input_num) == arm_num:
